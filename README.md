748b03484073397c8906a9b0118b62821eb848d5
0gust1 <augustin.chassine@gmail.com>
2014-06-30 00:52:47 +0200

    add strip options

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
 
-# Usage
-
 It's a little nodeJS CLI tool to :
 - request a webpage
-- inline and minify CSS and JS
+- inline (+minify) or strip CSS and JS
 - output the result webpage on the standard output
 
-You must have nodeJS installed on your system...
+# Usage
 
 - checkout the repository
 - In the directory type `npm install`.
 - and type `npm link`.
 
-Now you can do ``smoosh http://some.random.url --minify > result.txt``.
+Now you can do ``smoosh http://some.random.url --minify > result.txt``.  
+
 Type ``smoosh -h`` for help.
 
 ##TODO
 
-warning : The code is quite rude actually ;)
-
-- --strip, --strip_css & --strip_js options
 - ability to process files
 - testing
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var Q = require('q'); //utiliser q-io (https://github.com/kriskowal/q-io) ?
 //  var HTTP = require("q-io/http"); //<--
 
 var argv = require("nomnom")
+    .help("Get a webpage, inline all stylesheet and scripts, output on stdout")
     .options({
         url: {
             position: 0,
@@ -31,18 +32,24 @@ var argv = require("nomnom")
             abbr: '',
             flag: true,
             help: 'minify and inline CSS and inline JS (unless stripped)'
+        },
+        strip: {
+            abbr: 's',
+            flag: true,
+            help: 'strip all styles and scripts'
+        },
+        strip_css: {
+            abbr: '',
+            flag: true,
+            help: 'strip all styles'
+        },
+        strip_js: {
+            abbr: '',
+            flag: true,
+            help: 'strip all scripts'
         }
     })
     .parse();
-
-//CLI options :
-
-//minify
-//minify-js
-//minify-css
-
-//strip-js
-//strip-css
 
 var processJSInput = null;
 
@@ -76,17 +83,17 @@ var getRessource = function getRessource(url, callback) {
         req = https;
     }
 
-    req.get(url,function (res) {
+    req.get(url, function(res) {
         var data = "";
-        res.on("data", function (chunk) {
+        res.on("data", function(chunk) {
             data += chunk;
         });
-        res.on("end", function () {
+        res.on("end", function() {
             callback(data, url);
         });
-    }).on("error", function () {
-            callback(null);
-        });
+    }).on("error", function() {
+        callback(null);
+    });
 };
 
 //async http get - promise version
@@ -98,82 +105,79 @@ var getRessourceP = function getRessourceP(url) {
     if (Url.parse(url).protocol == "https:") {
         req = https;
     }
-    req.get(url,function (res) {
+    req.get(url, function(res) {
         var data = "";
-        res.on("data", function (chunk) {
+        res.on("data", function(chunk) {
             data += chunk;
         });
-        res.on("end", function () {
+        res.on("end", function() {
             deferred.resolve(data);
         });
-    }).on("error", function (e) {
-            deferred.reject(e);
-        });
+    }).on("error", function(e) {
+        deferred.reject(e);
+    });
 
     return deferred.promise;
 };
 
-/*var stripCSS = function stripCSS(){
-
- };
-
- var stripJS = function stripJS(){
- //this.$
- };
-
- var processingCSS = function processCSS(){
- var defer = Q.defer();
- var elem = $(this);
- var style = elem.attr('href');
- if (!style) {
- defer.reject();
- }
- if (style.match(/^\/\//)) {
- defer.reject();
- }
- // if (Url.parse(style).protocol) {
- //   return;
- // }
-
- style = Url.resolve(baseUrl, style);
-
- getRessourceP(style).then(function(data) {
- elem.replaceWith('<style>' + processCSSInput(data) + '</style>');
- defer.resolve();
- });
- return defer.promise;
- };
-
- var processingJS = function processJS(elem){
- //var elem = $(this);
- var defer = Q.defer();
- var script = elem.attr('src');
- if (!script) {
- defer.reject();
- }
- if (script.match(/^\/\//)) {
- defer.reject();
- }
- // if (Url.parse(script).protocol) {
- //   return;
- // }
-
- script = Url.resolve(baseUrl, script);
-
- getRessourceP(script).then(function(data) {
- elem.replaceWith('<script>' + processJSInput(data) + '</script>');
- defer.resolve();
- });
- return defer.promise;
- };*/
 
 //main function
 var processContent = function processContent(data, baseUrl) {
     //var deferred = new Deferred();
     var $ = cheerio.load(data);
-    var promises = [];
     //object containing the promises for stylesheets processing
-    var inliningStyle = $("link[rel='stylesheet']").map(processingCSS);
+    var inliningStyle;
+    //object containing the promises for script tags processing
+    var inliningScripts;
+
+    var promises = [];
+
+    if (argv.strip || argv.strip_css) {
+        inlingStyle = $("link[rel='stylesheet'], style").remove();
+    } else {
+        inliningStyle = $("link[rel='stylesheet']").map(processingCSS);
+    }
+
+
+
+    if (argv.strip || argv.strip_js) {
+        inliningScripts = $('script').remove();
+    } else {
+        inliningScripts = $('script[src]').map(function() {
+            var defer = Q.defer();
+            var elem = $(this);
+            var script = elem.attr('src');
+            if (!script) {
+                defer.reject();
+            }
+            if (script & script.match(/^\/\//)) {
+                defer.reject();
+            }
+            // if (Url.parse(script).protocol) {
+            //   return;
+            // }
+
+            script = Url.resolve(baseUrl, script);
+
+            getRessourceP(script).then(function(data) {
+                elem.replaceWith('<script>' + processJSInput(data) + '</script>');
+                defer.resolve();
+            });
+            return defer.promise;
+        });
+    }
+
+    //concatening all the promises into an array
+    if (inliningStyle && inliningStyle.length) {
+        promises = [].slice.call(inliningStyle);
+    }
+
+    promises.concat([].slice.call(inliningScripts));
+
+    //when all promises have resolved, output the processed html document
+    Q.all(promises).then(function() {
+        console.log($.html());
+    });
 
     function processingCSS() {
         var defer = Q.defer();
@@ -191,52 +195,16 @@ var processContent = function processContent(data, baseUrl) {
 
         style = Url.resolve(baseUrl, style);
 
-        getRessourceP(style).then(function (data) {
+        getRessourceP(style).then(function(data) {
             elem.replaceWith('<style>' + processCSSInput(data) + '</style>');
             defer.resolve();
         });
         return defer.promise;
     }
-
-    //object containing the promises for script tags processing
-    var inliningScripts = $('script[src]').map(function () {
-        var defer = Q.defer();
-        var elem = $(this);
-        var script = elem.attr('src');
-        if (!script) {
-            defer.reject();
-        }
-        if (script & script.match(/^\/\//)) {
-            defer.reject();
-        }
-        // if (Url.parse(script).protocol) {
-        //   return;
-        // }
-
-        script = Url.resolve(baseUrl, script);
-
-        getRessourceP(script).then(function (data) {
-            elem.replaceWith('<script>' + processJSInput(data) + '</script>');
-            defer.resolve();
-        });
-        return defer.promise;
-    });
-
-    //concatening all the promises into an array
-    promises = [].slice.call(inliningStyle);
-    promises.concat([].slice.call(inliningScripts));
-
-    //when all promises have resolved, output the processed html document
-    Q.all(promises).then(function () {
-        console.log($.html());
-    });
-
-
 };
 
-argv._.forEach(function (element) {
-    getRessource(element, processContent);
+//for each url in arguments (useful ? most of time, it will be 1)
+argv._.forEach(function(urlToSmoosh) {
+    getRessource(urlToSmoosh, processContent);
 
 });
-
-
